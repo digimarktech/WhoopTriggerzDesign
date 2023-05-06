da6e36119ebe4f78122b0c1d2c7e4eb7deffc5b5
Marc Aupont <mewsicmasta@gmail.com>
2023-05-06 10:18:51 -0400

    sets up gradient background view

--- WhoopTriggerzDesign/ContentView.swift
+++ WhoopTriggerzDesign/ContentView.swift
@@ -9,18 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            gradientBackgroundView
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension ContentView {
+    var gradientBackgroundView: some View {
+        LinearGradient(gradient: .init(colors: [
+            Color(red: 0.26, green: 0.28, blue: 0.31),
+            Color(red: 0.13, green: 0.14, blue: 0.15)
+        ]), startPoint: .top, endPoint: .bottom)
+        .edgesIgnoringSafeArea(.all)
     }
 }
