c7b036e7c2d12fd2c1fbb1087aebac71bdda7113
Marc Aupont <mewsicmasta@gmail.com>
2023-05-05 20:58:28 -0400

    Initial Commit

---
+++ WhoopTriggerzDesign/WhoopTriggerzDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoopTriggerzDesignApp.swift
+//  WhoopTriggerzDesign
+//
+//  Created by Marc Aupont on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WhoopTriggerzDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
